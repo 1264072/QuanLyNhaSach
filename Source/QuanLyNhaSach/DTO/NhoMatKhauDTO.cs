4ddc9a2feb2748ed3b77c93c44624f349c55edd2
Nguyen Xuan Hai <1264072@student.hcmus.edu.vn>
2015-06-19 10:18:54 +0700

    Commit Project

---
+++ Source/QuanLyNhaSach/DTO/NhoMatKhauDTO.cs
@@ -0,0 +1,36 @@
+﻿
+namespace DTO
+{
+    public class NhoMatKhauDTO
+    {
+        string uSERNAME;
+        string pASSWORD;
+        bool tRANGTHAI;
+
+        public bool TRANGTHAI
+        {
+            get { return tRANGTHAI; }
+            set { tRANGTHAI = value; }
+        }
+
+        public string PASSWORD
+        {
+            get { return pASSWORD; }
+            set { pASSWORD = value; }
+        }
+
+        public string USERNAME
+        {
+            get { return uSERNAME; }
+            set { uSERNAME = value; }
+        }
+
+        public NhoMatKhauDTO() { }
+        public NhoMatKhauDTO(string username, string password, bool status)
+        {
+            USERNAME = username;
+            PASSWORD = password;
+            TRANGTHAI = status;
+        }
+    }
+}
---
+++ Source/QuanLyNhaSach/DTO/TaiKhoanDTO.cs
@@ -0,0 +1,36 @@
+﻿
+namespace DTO
+{
+    public class TaiKhoanDTO
+    {
+        string uSERNAME;
+        string pASSWORD;
+        string mANV;
+
+        public string USERNAME
+        {
+            get { return uSERNAME; }
+            set { uSERNAME = value; }
+        }
+
+        public string PASSWORD
+        {
+            get { return pASSWORD; }
+            set { pASSWORD = value; }
+        }
+
+        public string MANV
+        {
+            get { return mANV; }
+            set { mANV = value; }
+        }
+
+        public TaiKhoanDTO() { }
+        public TaiKhoanDTO(string username, string password, string manv)
+        {
+            USERNAME = username;
+            PASSWORD = password;
+            MANV = manv;
+        }
+    }
+}
